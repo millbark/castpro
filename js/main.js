d0da438434004ab8e57f79dbd45421ecc85992fd
millbark <millbark@naver.com>
2024-02-13 00:34:25 +0900

    ver.6

--- js/main.js
+++ js/main.js
@@ -1,61 +1,59 @@
-$(function(){
-    $(window).scroll(function(){
-        if($(this).scrollTop()>100){
-          $('header h1 img').attr('src','images/header/logo.png');
-          $('header').addClass('on');
-        }else{
-            $('header h1 img').attr('src','images/header/white-logo.png');
-          $('header').removeClass('on');
-        }
-      }); 
-
-      $(".search").click(function (e) {
-        e.preventDefault();
-        let current = $(this).attr('data-id');
-        $('#' + current).addClass('on');
-        $("body").addClass("on");
-    });
-    $(".popup button").on("click", function () {
-      $(this).closest(".popup").removeClass("on");
-      $("body").removeClass("on");
+$(function () {
+  //header scroll
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('header h1 img').attr('src', 'images/header/logo.png');
+      $('header').addClass('on');
+    } else {
+      $('header h1 img').attr('src', 'images/header/white-logo.png');
+      $('header').removeClass('on');
+    }
   });
 
-
-    // $('.hello-station-tab-menu li').click(function(e){
-    //     e.preventDefault();
-    //     $('.hello-station-tab-menu li').removeClass('on');
-    //     $(this).addClass('on');
-    //     let current = $(this).attr('data-name');
-    //     $('.swiper-wrap').removeClass('on');
-    //     $('#' + current).addClass('on');
-    // });
-
-      $('.hello-station-tab-menu').click(function(e){
-        e.preventDefault();
-        $('.hello-station-tab-menu').removeClass('on');
-        $(this).addClass('on');
-        let current=$(this).attr('data-name');
-        $('.swiper-wrap').removeClass('on');
-        $('#' + current).addClass('on');
-      });
-
-      $("#inquiry .leaflet").click(function (e) {
-        e.preventDefault();
-        $('.popup-install-form').addClass('on'); 
-        $("body").addClass("on");
-    });
-    $(".popup-install-form button").on("click", function () {
-      $(this).closest(".popup-install-form").removeClass("on");
-      $("body").removeClass("on");
+  //search popup
+  $(".search").click(function (e) {
+    e.preventDefault();
+    let current = $(this).attr('data-id');
+    $('#' + current).addClass('on');
+    $("body").addClass("on");
+  });
+  $(".popup button").on("click", function () {
+    $(this).closest(".popup").removeClass("on");
+    $("body").removeClass("on");
   });
 
+  //hello-station
+  $('.hello-station-navi').click(function (e) {
+    e.preventDefault();
+    $('.hello-station-navi').removeClass('active');
+    $(this).addClass('active');
+
+    let current = $(this).attr('data-name');
+    $('.hello-station-slide').removeClass('on');
+    $('#' + current).addClass('on');
+
+    $('#' + current).slick('setPosition');
+});
+
+  //inquiry
+  $("#inquiry .leaflet").click(function (e) {
+    e.preventDefault();
+    $('.popup-install-form').addClass('on');
+    $("body").addClass("on");
+  });
+  $(".popup-install-form button").on("click", function () {
+    $(this).closest(".popup-install-form").removeClass("on");
+    $("body").removeClass("on");
+  });
+
+  //#quick-menu
   $("#quick-menu li").mouseenter(function (e) {
     e.preventDefault();
     $('#quick-menu li').removeClass('on');
     $(this).addClass('on');
-});
-$("#quick-menu li").mouseleave(function (e) {
-  e.preventDefault();
-  $('#quick-menu li').removeClass('on');
-});
+  });
+  $("#quick-menu li").mouseleave(function (e) {
+    e.preventDefault();
+    $('#quick-menu li').removeClass('on');
+  });
 })//ready
---
+++ js/slick.js
@@ -0,0 +1,31 @@
+$(function () {
+  $('#hello-station-1').slick({
+    dots:true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 2
+  });
+  $('#hello-station-2').slick({
+    dots:true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 2
+  });
+  $('#hello-station-3').slick({
+    dots:true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 2
+  });
+  $('#hello-station-4').slick({
+    dots:true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 2
+  });
+
+  // 나머지 코드는 동일하게 유지됩니다.
+});//ready
+
+
+
